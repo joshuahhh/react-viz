3ef086882b5a6143c01a53d660c780c0575d26e1
Joshua Horowitz <joshuah@alum.mit.edu>
2015-10-20 16:23:42 -0700

    rv: switch to better method of importing in all.jsx

--- all.jsx
+++ all.jsx
@@ -1,39 +1,19 @@
-import AnimationLoop from './AnimationLoop';
-import Axis from './Axis';
-import Circle from './Circle';
-import ClipPath from './ClipPath';
-import Draggable from './Draggable';
-import G from './G';
-import Hoverable from './Hoverable';
-import Json from './Json';
-import Line from './Line';
-import LoBox from './LoBox';
-import LoCol from './LoCol';
-import LoMargins from './LoMargins';
-import LoRow from './LoRow';
-import Path from './Path';
-import PlotRegion from './PlotRegion';
-import Rect from './Rect';
-import Svg from './Svg';
-import Text from './Text';
-
-export default {
-  AnimationLoop,
-  Axis,
-  Circle,
-  ClipPath,
-  Draggable,
-  G,
-  Hoverable,
-  Json,
-  Line,
-  LoBox,
-  LoCol,
-  LoMargins,
-  LoRow,
-  Path,
-  PlotRegion,
-  Rect,
-  Svg,
-  Text,
-};
+export {default as AnimationLoop} from './AnimationLoop';
+export {default as Axis} from './Axis';
+export {default as Circle} from './Circle';
+export {default as ClipPath} from './ClipPath';
+export {default as Draggable} from './Draggable';
+export {default as G} from './G';
+export {default as Hoverable} from './Hoverable';
+export {default as Json} from './Json';
+export {default as Line} from './Line';
+export {default as LoBox} from './LoBox';
+export {default as LoCol} from './LoCol';
+export {default as LoMargins} from './LoMargins';
+export {default as LoRow} from './LoRow';
+export {default as Mask} from './Mask';
+export {default as Path} from './Path';
+export {default as PlotRegion} from './PlotRegion';
+export {default as Rect} from './Rect';
+export {default as Svg} from './Svg';
+export {default as Text} from './Text';
